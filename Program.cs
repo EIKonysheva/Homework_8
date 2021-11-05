cab41a68320268519d1390372dcff4e5efdfb260
Лена Конышева <EIKonysheva@stud.kpfu.ru>
2021-11-05 22:45:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+            bank.PutItOnTheAccount(787878m);
+            bank.Dispose("bankTransy.txt");
+            Song mySong = new Song();
+        }
+    }
+}
---
+++ Song.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_8
+{
+    class Song
+    {
+        private string name;
+        private string author;
+        private Song prev;
+        public Song() 
+        {
+        }
+        public Song(string name, string author)
+        {
+            this.name = name;
+            this.author = author;
+        }
+        public Song(string name, string author, Song prev)
+        {
+            this.name = name;
+            this.author = author;
+            this.prev = prev;
+        }
+        public string Title()
+        {
+            return name + " - " + author;
+        }
+        public override bool Equals(object d)
+        {
+            bool b = (d as Song).name.Equals(name) && (d as Song).author.Equals(author);
+            return b;
+            //return d != null &&
+            //name.Equals(((Song)d).name) &&
+            //author.Equals(((Song)d).author);
+        }
+    }
+}
